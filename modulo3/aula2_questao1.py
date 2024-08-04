d99d50f24ecd8492fe426041a981dfb4fc24b010
filmes favoritos <lucas.rodrigues@pdbomdespacho.com.br>
2024-08-04 14:46:02 -0300

    Add files via upload

---
+++ modulo3/aula2_questao1.py
@@ -0,0 +1,4 @@
+idade_juliana = int(input("qual a idade da Juliana? "))
+idade_Cris = int(input("qual a idade da Cris ? "))
+maiores_de_idade = idade_juliana > 17 and idade_Cris > 17
+print(maiores_de_idade)
